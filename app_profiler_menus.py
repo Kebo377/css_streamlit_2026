f88a0673bee0c9956b65a0f423a3e5a7dabebbda
Kebo377 <2020512956@ufs4life.ac.za>
2026-01-31 14:43:20 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -48,7 +48,7 @@ if menu == "Researcher Profile":
     st.write(f"**Institution:** {institution}")
     
     st.image(
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+    r"C:\Users\Ndlovuss2\Downloads\IMG-20240115-WA0004.jpg",
     caption="Nature (Pixabay)"
 )
 
@@ -134,4 +134,5 @@ elif menu == "Contact":
     email = "kebogilesesana@gmail.com"
 
     st.write(f"You can reach me at {email}.")
+
 
