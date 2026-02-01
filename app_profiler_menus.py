c99a1e1739f7e18217a43b3ca2b96d63f08db5ff
Kebo377 <2020512956@ufs4life.ac.za>
2026-02-01 15:56:02 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -55,15 +55,15 @@ if menu == "Researcher Profile":
 
 st.title("Educational Qualification")
 
-#elif menu == "Educational_Qualifications":
-#    st.title("Educational_Qualifications")
- #   st.sidebar.header("Upload and Filter")
+elif menu == "Educational_Qualifications":
+    st.title("Educational_Qualifications")
+    st.sidebar.header("Upload and Filter")
 
     # Upload Educational_Qualifications file
-   # uploaded_file = st.file_uploader("Upload a pdf  of your educational_qualifications", type="Educational Qualifications")
-   # if uploaded_file:
-    #    educational_qualifications = pd.read_csv(uploaded_file)
-     #   st.dataframe(Educational_Qualifications)
+    uploaded_file = st.file_uploader("Upload a pdf  of your educational_qualifications", type="Educational Qualifications")
+    if uploaded_file:
+        educational_qualifications = pd.read_csv(uploaded_file)
+        st.dataframe(Educational_Qualifications)
 
         # Add filtering for year or keyword
         keyword = st.text_input("Filter by keyword", "")
@@ -138,6 +138,7 @@ elif menu == "Contact":
     email = "kebogilesesana@gmail.com"
 
     st.write(f"You can reach me at {email}.")
+
 
 
 
