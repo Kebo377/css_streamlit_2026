3740c0f469bee5abba86facdd4bf14580012c92e
Kebo377 <2020512956@ufs4life.ac.za>
2026-02-01 15:53:53 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -60,7 +60,7 @@ st.title("Educational Qualification")
  #   st.sidebar.header("Upload and Filter")
 
     # Upload Educational_Qualifications file
-    uploaded_file = st.file_uploader("Upload a pdf  of your educational_qualifications", type="Educational Qualifications")
+   # uploaded_file = st.file_uploader("Upload a pdf  of your educational_qualifications", type="Educational Qualifications")
     if uploaded_file:
         educational_qualifications = pd.read_csv(uploaded_file)
         st.dataframe(Educational_Qualifications)
@@ -138,6 +138,7 @@ elif menu == "Contact":
     email = "kebogilesesana@gmail.com"
 
     st.write(f"You can reach me at {email}.")
+
 
 
 
